e51204d5241e12c908cd7d7c60b29c4f489917cb
BlueDespero <cezarytroska@gmail.com>
2025-08-11 23:24:06 +0200

    Make balls snap into the grid

--- ball.gd
+++ ball.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 var dragging: bool = false
+var spawn_point: Vector2i
+@export var player_board: TileMapLayer
+
+func _ready() -> void:
+	spawn_point = Vector2i(self.position.x, self.position.y)
 
 func _process(delta: float) -> void:
 	if dragging:
@@ -14,3 +19,20 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 			dragging = true
 		if event.is_released():
 			dragging = false
+			snap_to_grid()
+			
+func snap_to_grid() -> void:
+	var chosen_cell_coords : Vector2i =  player_board.local_to_map(player_board.get_local_mouse_position())
+	var chosen_cell : TileData = player_board.get_cell_tile_data(chosen_cell_coords)
+	# Cell not in Player Board
+	if not chosen_cell:
+		return_to_spawn()
+		return
+	# Cell in Player board
+	snap_to_tile(chosen_cell_coords)
+	
+func return_to_spawn() -> void:
+	self.position = spawn_point
+
+func snap_to_tile(cell_coords: Vector2i) -> void:
+		self.position = player_board.to_global(player_board.map_to_local(cell_coords))
